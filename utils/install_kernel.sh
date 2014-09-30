0c11c0ccd227fdef6b889aa858a7fd3f3a578e5f
Arnaud Loonstra <arnaud@spaero.org>
2014-09-30 18:37:11 +0200

    added depmod module dependency generation

--- utils/install_kernel.sh
+++ utils/install_kernel.sh
@@ -6,6 +6,9 @@ if [ -z $1 ]; then
     exit 1
 fi
 
+VERSION=`echo $1| cut -d "-" -f3,4,5 | cut -d "_" -f1`
+echo "VERSION  = $VERSION"
+
 rm -rf debian-binary data.* control.* usr/ lib/ boot/ || true
 #wget http://security.debian.org/debian-security/pool/updates/main/l/linux/linux-image-3.2.0-4-486_3.2.57-3+deb7u1_i386.deb
 
@@ -13,4 +16,5 @@ ar -x $1
 unxz data.tar.xz
 tar -xf data.tar
 cp -r lib jessie-net/
+depmod -b jessie-net -F boot/System.map-$VERSION $VERSION
 rm -rf data.tar control.tar.gz lib/ usr/ debian-binary
